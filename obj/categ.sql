3ecdfa6300e29f87eebd2c6997686e3ab884b2bc
catatete <catalin.jecu@gmail.com>
2016-05-18 18:56:35 +0300

    v1
    obiecte
    tari, categ

---
+++ obj/categ.sql
@@ -0,0 +1,10 @@
+DROP TABLE categ;
+/
+CREATE TABLE categ
+  ( tip_res VARCHAR2(20) PRIMARY KEY
+  );
+/
+INSERT INTO categ VALUES ('pizzerie');
+INSERT INTO categ VALUES ('benzinarie');
+/
+--------------------------------
---
+++ obj/resurse_oop,tari&constrangeriResObj.sql
@@ -0,0 +1,31 @@
+--sequence e un iteraror si se acceseaza cu:
+--seq_resurse.currval , seq_resurse.nextval
+DROP SEQUENCE seq_resurse;
+/
+CREATE SEQUENCE seq_resurse
+MINVALUE 1
+START WITH 1
+INCREMENT BY 1
+CACHE 10;
+/
+DROP TABLE resurse_oop;
+Create Table resurse_oop(id_res NUMBER(10), obiect RESURSA);
+/
+alter table resurse_oop
+   add constraint FK_tip_res
+      foreign key (obiect.tip_res) references categ (tip_res);
+/
+Drop table Tari;
+/
+CREATE TABLE TARI 
+(
+  id_tara CHAR(2 BYTE) NOT NULL 
+, nume_tara VARCHAR2(40 BYTE)  
+, CONSTRAINT ID_TARA_PK PRIMARY KEY 
+  (  ID_TARA   ) ENABLE 
+);
+/
+alter table resurse_oop
+   add constraint FK_id_tara
+      foreign key (obiect.id_tara) references tari(id_tara);
+/
