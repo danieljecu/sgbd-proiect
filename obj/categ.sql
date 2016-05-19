f84d500e6ec1db2006aab35f42cde2eaa780b685
catatete <catalin.jecu@gmail.com>
2016-05-19 14:32:06 +0300

    v3 obiecte noi + insert in categ

--- obj/categ.sql
+++ obj/categ.sql
@@ -5,6 +5,11 @@ CREATE TABLE categ
   );
 /
 INSERT INTO categ VALUES ('pizzerie');
-INSERT INTO categ VALUES ('benzinarie');
+INSERT INTO categ VALUES ('banca');
+INSERT INTO categ VALUES ('magazin');
+INSERT INTO categ VALUES ('scoala');
+INSERT INTO categ VALUES ('dentist');
+INSERT INTO categ VALUES ('aeroport');
+INSERT INTO categ VALUES ('farmacie');
 /
 --------------------------------
